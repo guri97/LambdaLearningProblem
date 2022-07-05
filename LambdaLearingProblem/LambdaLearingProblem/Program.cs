5d8e2f42c17d762892f1fd28adcd427614e5c19a
guri97 <105732963+guri97@users.noreply.github.com>
2022-07-05 18:52:36 +0530

    [gurpreet] UC2_RetriveTop2Person

--- LambdaLearingProblem/LambdaLearingProblem/Program.cs
+++ LambdaLearingProblem/LambdaLearingProblem/Program.cs
@@ -4,6 +4,7 @@
     {
         public static void Main(string[] args)
         {
+            //uc1
             List<Person> listPersonInCity = new List<Person>();
             listPersonInCity.Add(new Person("2345671", "anu", "1st cross 8th main BNG", 55));
             listPersonInCity.Add(new Person("2345672", "anaga", "2st cross 7th main BNG", 15));
@@ -14,6 +15,14 @@
             listPersonInCity.Add(new Person("2345677", "shalini", "7st cross 2th main BNG", 85));
             listPersonInCity.Add(new Person("2345678", "sheela", "8st cross 1th main BNG", 25));
             listPersonInCity.Add(new Person("2345679", "asha", "9st cross 0th main BNG", 95));
+
+            //uc2
+            Console.WriteLine("\n______________________________________");
+            Console.WriteLine("Retriving top 2 aged persons from the list who are below 60 years");
+            foreach (Person person in listPersonInCity.FindAll(e => (e.Age <= 60)).Take(2).ToList())
+            {
+                Console.WriteLine("Name:" + person.Name + "\t\tAge:" + person.Age);
+            }
         }
     }
 }
