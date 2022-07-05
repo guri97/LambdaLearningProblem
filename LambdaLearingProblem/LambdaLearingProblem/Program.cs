e3c5cbeceaeb512cfad9ce06029247d974c5f677
guri97 <105732963+guri97@users.noreply.github.com>
2022-07-05 18:47:46 +0530

    [gurpreet] UC1_AddPersonDataWithDefault

---
+++ LambdaLearingProblem/LambdaLearingProblem/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Day24_LambdaLearningProblem
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            List<Person> listPersonInCity = new List<Person>();
+            listPersonInCity.Add(new Person("2345671", "anu", "1st cross 8th main BNG", 55));
+            listPersonInCity.Add(new Person("2345672", "anaga", "2st cross 7th main BNG", 15));
+            listPersonInCity.Add(new Person("2345673", "arya", "3st cross 6th main BNG", 35));
+            listPersonInCity.Add(new Person("2345674", "janu", "4st cross 5th main BNG", 65));
+            listPersonInCity.Add(new Person("2345675", "anjan", "5st cross 4th main BNG", 66));
+            listPersonInCity.Add(new Person("2345676", "anupama", "6st cross 3th main BNG", 55));
+            listPersonInCity.Add(new Person("2345677", "shalini", "7st cross 2th main BNG", 85));
+            listPersonInCity.Add(new Person("2345678", "sheela", "8st cross 1th main BNG", 25));
+            listPersonInCity.Add(new Person("2345679", "asha", "9st cross 0th main BNG", 95));
+        }
+    }
+}
